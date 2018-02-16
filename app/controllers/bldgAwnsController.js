34cd332169a88dd721e93a1882bb6b44f44f1246
Isai Solis <solis.isai@gmail.com>
2018-02-16 16:46:01 -0600

    Added ajax function

--- app/controllers/bldgAwnsController.js
+++ app/controllers/bldgAwnsController.js
@@ -4,7 +4,26 @@ var express = require("express");
 var router = express.Router();
 
 // Add the API calls here
+var facilityAddress = ""; 
+var queryURLBase = "https://data.austintexas.gov/resource/5mvc-79r6.json" 
 
+function runQuery(queryURL){
+  $.ajax({
+      url: queryURL,
+      method: "GET"
+  }).done(function(aegbData){
+    console.log("------------------------------------");
+    console.log("URL: " + queryURL);
+    console.log("------------------------------------");
+    console.log(aegbData);
+    console.log("------------------------------------");
 
+    for (var i=0; i<aegbData.length; i++){
+        if (aegbData[i].facility_address === facilityAddress){
+
+        }
+    }
+  })    
+}
 // Export routes for server.js to use.
 module.exports = router;
